85bc15b7d1e8f87dc1a0105165aa31e66fed7edc
Griffin Rademacher <griffin.rademacher@pathai.com>
2019-08-25 14:33:57 -0400

    full implementation of exits/entrances

--- src/plaintext_parse.py
+++ src/plaintext_parse.py
@@ -1,13 +1,33 @@
-import re
 import json
+import re
+import string
 
 from flask import Flask, render_template
 
 
 ACT_BREAK = re.compile("^\s*act\s+\S+")
 ACTOR_MOVEMENTS = re.compile("(enter|exit|exeunt|re-enter)")
-ACTOR_NAMES = re.compile("[A-Z]+")
+ACTOR_NAMES = re.compile("\\b[A-Z]+\\b")
 SCENE_BREAK = re.compile("^\s*scene\s+\S+")
+
+
+def exeunt(current_characters, changed_characters):
+    return []
+
+
+def exit(current_characters, changed_characters):
+    for character in changed_characters:
+        current_characters.remove(character)
+    return current_characters
+
+
+def enter(current_characters, changed_characters):
+    for character in changed_characters:
+        current_characters.append(character)
+    return current_characters
+
+
+MOVEMENT_FUNCS = {"enter": enter, "re-enter": enter, "exit": exit, "exeunt": exeunt}
 
 app = Flask(__name__)
 
@@ -15,6 +35,7 @@ app = Flask(__name__)
 def parse_plaintext_script(path):
     output = {}
     french_scene = 1
+    current_characters = []
     with open(path, "r") as script:
         for line in script:
             if is_change(ACT_BREAK, line):
@@ -25,10 +46,25 @@ def parse_plaintext_script(path):
                 output[current_act][current_scene] = {}
                 french_scene = 1
             elif is_change(ACTOR_MOVEMENTS, line):
-                actors = [word for word in line.strip().split(" ") if re.match(ACTOR_NAMES, word)]
-                output[current_act][current_scene][str(french_scene)] = actors
+                print(json.dumps(output, indent=4))
+                changed_characters = [
+                    word.strip(string.punctuation)
+                    for word in line.strip().split(" ")
+                    if re.match(ACTOR_NAMES, word)
+                ]
+                movement = get_change(ACTOR_MOVEMENTS, line)
+                current_characters = handle_movement(
+                    movement, current_characters, changed_characters
+                )
+                if any(current_characters):
+                    output[current_act][current_scene][str(french_scene)] = current_characters[:]
                 french_scene += 1
     return output
+
+
+def handle_movement(movement, current_characters, changed_characters):
+    movement = movement.lower()
+    return MOVEMENT_FUNCS[movement](current_characters, changed_characters)
 
 
 def is_change(pattern, line):
